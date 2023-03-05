b53a0f84838341cc9b5f8da1f327f4720ff1ab23
mojoee <47809126+mojoee@users.noreply.github.com>
2023-03-05 12:45:54 +0800

    added comments for todo

--- minion_chess/src/main.py
+++ minion_chess/src/main.py
@@ -12,6 +12,10 @@ def main():
     p2 = Side(config.screen_height/8*7, config.screen_height/8*5)
     pg.init()
     while game_loop:
+        #TODO: do not make the screen everytime, rather create a container similar to p1 
+        # that will draw the objects inside. Then later, we can access the tiles in that
+        # container more easily.
+        # make a design plan with figma
         screen.MakeScreen()
         p1.draw(screen.screen)
         p2.draw(screen.screen)
